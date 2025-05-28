32bd8ef307d4760c28024b71349bf22f8bca1d27
Maya Jairam <mj20@princeton.edu>
2025-05-28 00:04:49 -0400

    changes

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,17 @@
+// src/app/api/auth/[...nextauth]/route.js
+
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export const GET = handler;
+export const POST = handler;
