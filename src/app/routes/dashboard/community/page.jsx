fce6d1b4795c5a66af8e6b420b5642c76fc88e7d
Lindsay Allen <159066551+lkallen@users.noreply.github.com>
2025-05-29 18:48:13 -0400

    basic navbar with links to routes

--- src/app/routes/dashboard/community/page.jsx
+++ src/app/routes/dashboard/community/page.jsx
@@ -1,7 +1,7 @@
 export default function CommunityPage() {
   return (
     <div>
-      <p>community page goes here</p>
+      <p>This is the community page</p>
     </div>
   );
 }
--- src/app/routes/dashboard/layout.jsx
+++ src/app/routes/dashboard/layout.jsx
@@ -1,7 +1,6 @@
 export default function DashboardLayout({ children }) {
   return (
     <div>
-      <p>shared layout for dashboard and its sub-routes goes here</p>
       <div>{children}</div>
     </div>
   );
