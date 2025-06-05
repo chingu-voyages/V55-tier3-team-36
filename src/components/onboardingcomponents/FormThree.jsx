a2c850204d824831cc4f6ffd7955779d2493966b
Lindsay Allen <159066551+lkallen@users.noreply.github.com>
2025-06-05 13:49:42 -0400

    added edit functionality to all parts of step 3 and disabled save option if in edit mode

--- src/components/onboardingcomponents/FormThree.jsx
+++ src/components/onboardingcomponents/FormThree.jsx
@@ -1,60 +1,114 @@
 "use client";
 import { useEffect, useState } from "react";
 import { MdOutlineEdit } from "react-icons/md";
+import { MdOutlineCancel } from "react-icons/md";
+import { MdCheckCircleOutline } from "react-icons/md";
 import ErrorMessage from "./ErrorMessage";
 
 export default function FormThree() {
   const [goalData, setGoalData] = useState("");
   const [behaviorData, setBehaviorData] = useState("");
   const [whenData, setWhenData] = useState("");
+  const [isGoalEditing, setIsGoalEditing] = useState(false);
+  const [isBehaviorEditing, setIsBehaviorEditing] = useState(false);
+  const [isWhenEditing, setIsWhenEditing] = useState(false);
+  const [updatedGoal, setUpdatedGoal] = useState("");
+  const [updatedBehavior, setUpdatedBehavior] = useState("");
+  const [updatedWhen, setUpdatedWhen] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [isSaveDisabled, setIsSaveDisabled] = useState(false);
 
   function getGoalData() {
     const goal = sessionStorage.getItem("goal");
     setGoalData(goal);
   }
-
   function getBehaviorData() {
     const behavior = sessionStorage.getItem("behavior");
     setBehaviorData(behavior);
   }
-
   function getWhenData() {
     const when = sessionStorage.getItem("when");
     setWhenData(when);
   }
-
   useEffect(() => {
     getGoalData();
     getBehaviorData();
     getWhenData();
   }, []);
-
-  // add save function - SAVE FUNCTION SHOULD: update database, clear session storage
-  // need to add functionality to edit icons
-  // validate on this page only?
-
-  const [isEditing, setIsEditing] = useState(false);
+  useEffect(() => {
+    setUpdatedGoal(goalData);
+  }, [goalData]);
+  useEffect(() => {
+    setUpdatedBehavior(behaviorData);
+  }, [behaviorData]);
+  useEffect(() => {
+    setUpdatedWhen(whenData);
+  }, [whenData]);
 
   function handleGoalChange(event) {
-    setGoalData(event.target.value);
+    setUpdatedGoal(event.target.value);
+  }
+  function handleBehaviorChange(event) {
+    setUpdatedBehavior(event.target.value);
+  }
+  function handleWhenChange(event) {
+    setUpdatedWhen(event.target.value);
   }
 
-  const [isError, setIsError] = useState(false);
-
-  function handleRoutineSave(event) {
+  function handleGoalSave(event) {
     event.preventDefault();
-    if (goalData === "") {
+    if (updatedGoal === "") {
       setIsError(true);
     } else {
       setIsError(false);
-      const goal = sessionStorage.setItem("goal", goalData);
-      setIsEditing(false);
+      sessionStorage.setItem("goal", updatedGoal);
+      getGoalData();
+      setIsGoalEditing(false);
+    }
+  }
+  function handleBehaviorSave(event) {
+    event.preventDefault();
+    sessionStorage.setItem("behavior", updatedBehavior);
+    getBehaviorData();
+    setIsBehaviorEditing(false);
+  }
+  function handleWhenSave(event) {
+    event.preventDefault();
+    sessionStorage.setItem("when", updatedWhen);
+    getWhenData();
+    setIsWhenEditing(false);
+  }
+
+  function cancelGoalUpdate() {
+    setUpdatedGoal(goalData);
+    setIsGoalEditing(false);
+  }
+  function cancelBehaviorUpdate() {
+    setUpdatedBehavior(behaviorData);
+    setIsBehaviorEditing(false);
+  }
+  function cancelWhenUpdate() {
+    setUpdatedWhen(whenData);
+    setIsWhenEditing(false);
+  }
+
+  function checkIfEditing() {
+    if (isGoalEditing || isBehaviorEditing || isWhenEditing) {
+      setIsSaveDisabled(true);
+    } else {
+      setIsSaveDisabled(false);
     }
   }
 
-  // to do - allow edits for behavior and when
+  useEffect(() => {
+    checkIfEditing();
+  }, [isGoalEditing, isBehaviorEditing, isWhenEditing]);
 
-  //  function for save to console log all routine
+  const enabledSaveClassName =
+    "cursor-pointer bg-blue-500 rounded-lg w-full p-2 mt-10 text-white text-sm";
+  const disabledSaveClassName =
+    "bg-gray-500 rounded-lg w-full p-2 mt-10 text-white text-sm";
+
   function handleSave() {
     console.log(goalData);
     console.log(behaviorData);
@@ -63,68 +117,152 @@ export default function FormThree() {
 
   return (
     <div className=" w-3/4 ">
+      {/* GOAL DATA */}
       <div className="flex  gap-10">
         <div className="w-3/4 block text-lg font-bold  text-gray-900 ">
           Name of implementation intention
         </div>
         <div className="">
-          <button className="cursor-pointer" onClick={() => setIsEditing(true)}>
-            <MdOutlineEdit />
-          </button>
+          {!isGoalEditing && (
+            <button
+              className="cursor-pointer"
+              onClick={() => setIsGoalEditing(true)}
+            >
+              <MdOutlineEdit />
+            </button>
+          )}
+          {isGoalEditing && (
+            <div className=" flex text-lg gap-1 items-center">
+              <button
+                className="cursor-pointer"
+                onClick={() => cancelGoalUpdate()}
+              >
+                <MdOutlineCancel />
+              </button>
+              <button
+                className="cursor-pointer"
+                onClick={() => handleGoalSave(event)}
+              >
+                <MdCheckCircleOutline />
+              </button>
+            </div>
+          )}
         </div>
       </div>
 
       <div className="mb-6">
-        {!isEditing && <p className=" text-gray-500">{goalData}</p>}
-        {isEditing && (
+        {!isGoalEditing && <p className=" text-gray-500 p-2.5">{goalData}</p>}
+        {isGoalEditing && (
           <input
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block w-full p-2.5  "
-            value={goalData}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block p-2.5 w-full "
+            value={updatedGoal}
             onChange={handleGoalChange}
           />
         )}
         {isError && <ErrorMessage />}
       </div>
 
-      <div className="flex gap-10">
-        <div className="w-3/4  block text-lg font-bold  text-gray-900 ">
+      {/* BEHAVIOR DATA */}
+      <div className="flex  gap-10">
+        <div className="w-3/4 block text-lg font-bold  text-gray-900 ">
           Behavior
         </div>
         <div className="">
-          <MdOutlineEdit />
+          {!isBehaviorEditing && (
+            <button
+              className="cursor-pointer"
+              onClick={() => setIsBehaviorEditing(true)}
+            >
+              <MdOutlineEdit />
+            </button>
+          )}
+          {isBehaviorEditing && (
+            <div className=" flex text-lg gap-1 items-center">
+              <button
+                className="cursor-pointer"
+                onClick={() => cancelBehaviorUpdate()}
+              >
+                <MdOutlineCancel />
+              </button>
+              <button
+                className="cursor-pointer"
+                onClick={() => handleBehaviorSave(event)}
+              >
+                <MdCheckCircleOutline />
+              </button>
+            </div>
+          )}
         </div>
       </div>
-      <p className="mb-6 text-gray-500">{behaviorData}</p>
 
+      <div className="mb-6">
+        {!isBehaviorEditing && (
+          <p className=" text-gray-500 p-2.5">{behaviorData}</p>
+        )}
+        {isBehaviorEditing && (
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block p-2.5 w-full "
+            value={updatedBehavior}
+            onChange={handleBehaviorChange}
+          />
+        )}
+      </div>
+
+      {/* WHEN DATA */}
       <div className="flex  gap-10">
-        <div className="w-3/4  block text-lg font-bold  text-gray-900 ">
+        <div className="w-3/4 block text-lg font-bold  text-gray-900 ">
           When
         </div>
-
         <div className="">
-          <MdOutlineEdit />
+          {!isWhenEditing && (
+            <button
+              className="cursor-pointer"
+              onClick={() => setIsWhenEditing(true)}
+            >
+              <MdOutlineEdit />
+            </button>
+          )}
+          {isWhenEditing && (
+            <div className=" flex text-lg gap-1 items-center">
+              <button
+                className="cursor-pointer"
+                onClick={() => cancelWhenUpdate()}
+              >
+                <MdOutlineCancel />
+              </button>
+              <button
+                className="cursor-pointer"
+                onClick={() => handleWhenSave(event)}
+              >
+                <MdCheckCircleOutline />
+              </button>
+            </div>
+          )}
         </div>
       </div>
 
-      <p className="mb-6 text-gray-500">{whenData}</p>
+      <div className="mb-6">
+        {!isWhenEditing && <p className=" text-gray-500 p-2.5">{whenData}</p>}
+        {isWhenEditing && (
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block p-2.5 w-full "
+            value={updatedWhen}
+            onChange={handleWhenChange}
+          />
+        )}
+      </div>
 
+      {/* SAVE BUTTON */}
       <div>
-        {isEditing && (
-          <button
-            className="cursor-pointer bg-blue-500 rounded-lg w-full p-2 mt-10 text-white text-sm"
-            onClick={handleRoutineSave}
-          >
-            Save Updated Routine
-          </button>
-        )}
-        {!isEditing && (
-          <button
-            className="cursor-pointer bg-blue-500 rounded-lg w-full p-2 mt-10 text-white text-sm"
-            onClick={handleSave}
-          >
-            SAVE
-          </button>
-        )}
+        <button
+          onClick={handleSave}
+          disabled={isSaveDisabled}
+          className={
+            isSaveDisabled ? disabledSaveClassName : enabledSaveClassName
+          }
+        >
+          SAVE
+        </button>
       </div>
     </div>
   );
