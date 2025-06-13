00d0422ccd11e85bcc342e10419db6ac3d4eb60b
Lindsay Allen <159066551+lkallen@users.noreply.github.com>
2025-06-13 10:17:27 -0400

    added sign out functionality to dashboard

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 import StreakStats from "./StreakStats";
 
@@ -8,7 +8,10 @@ export default function DashboardHeader() {
 
   return (
     <div className="bg-blue-100  h-full px-6 pt-6 pb-8">
-      <button className="mb-4 flex justify-self-end cursor-pointer px-4 py-2 border rounded-full text-blue-800 border-blue-800 hover:bg-blue-50 transition text-xs">
+      <button
+        className="mb-4 flex justify-self-end cursor-pointer px-4 py-2 border rounded-full text-blue-800 border-blue-800 hover:bg-blue-50 transition text-xs"
+        onClick={() => signOut({ callbackUrl: "/" })}
+      >
         SIGN OUT
       </button>
       <div className=" flex justify-between items-center p-8">
